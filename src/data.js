0595997c81bcb3d45c4acc0d128560b2b9592321
hesham <hesham@heshams-iMac.local>
2021-01-22 06:26:32 +0300

    My first timeline

---
+++ src/data.js
@@ -0,0 +1,44 @@
+const data = [
+  {
+    text: "",
+    date: "",
+    category: {
+      tag: "medium",
+      color: "#018f69",
+    },
+  },
+  {
+    text: "",
+    date: "",
+    category: {
+      tag: "medium",
+      color: "#018f69",
+    },
+  },
+  {
+    text: "",
+    date: "",
+    category: {
+      tag: "medium",
+      color: "#018f69",
+    },
+  },
+  {
+    text: "",
+    date: "",
+    category: {
+      tag: "medium",
+      color: "#018f69",
+    },
+  },
+  {
+    text: "",
+    date: "",
+    category: {
+      tag: "medium",
+      color: "#018f69",
+    },
+  },
+];
+
+export default data;
